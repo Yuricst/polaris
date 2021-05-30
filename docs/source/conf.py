a068ee9f619d1248b2ff097019419d418f35d92b
Yuricst <yuri.shimane@gmail.com>
2021-05-30 14:32:41 -0400

    docs update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Yuri'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
@@ -76,7 +76,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
