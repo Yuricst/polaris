cfe83a5bd8f53f0f3d5f9b67ccfae3860a89fb88
Yuricst <yuri.shimane@gmail.com>
2021-05-30 13:15:38 -0400

    setup.py update

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
 from setuptools import setup
 
 
-requires = ["numba>=0.51.2"]
+requires = [
+    'matplotlib>=3.3.2',
+    'numpy>=1.20.0',
+    'numba>=0.51.2',
+    'pandas>=1.1.4',
+    'scipy>=1.5.2'
+]
 
 
 setup(
     name='polaris',
-    version='0.1',
-    description='Awesome library',
+    version='0.1.0',
+    description='Astrodynamics library in Python',
     url='https://github.com/Yuricst/polaris',
-    author='Yuri',
+    author='Yuri Shimane',
     author_email='yuri.shimane@gmail.com',
     license='MIT',
-    keywords='astrodynamics',
     packages=[
-        "polaris",
-        "polaris.Propagator",
-        "polaris.R3BP",
-        "polaris.Keplerian",
-        "polaris.Coordinates",
+        'polaris',
+        'polaris.Propagator',
+        'polaris.R3BP',
+        'polaris.Keplerian',
+        'polaris.Coordinates',
     ],
     install_requires=requires,
     classifiers=[
         'Programming Language :: Python :: 3.7',
     ],
 )
+
