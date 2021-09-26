f5fd707847aa249762269f434dd3f03e6cc8c484
Yuri <50186716+Yuricst@users.noreply.github.com>
2021-09-26 15:51:56 -0400

    Update __init__.py

--- polaris/Keplerian/__init__.py
+++ polaris/Keplerian/__init__.py
@@ -7,7 +7,7 @@ Functions associated with Keplerian dynamics
 from ._rootSolve import __newtonRaphson
 from ._rotationMatrices import *
 from ._orbitalElements import *
-from ._statevec2elements import sv2elts
+from ._statevec2elements import sv2elts, print_elts
 from ._elements2statevec import elts2sv
 from ._miscExpressions import get_synodic_period, get_energy, get_c3, get_hohmann_cost
 
