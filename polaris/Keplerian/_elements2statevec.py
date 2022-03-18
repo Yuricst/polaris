e10f76cd69c3d8c4df0a87d827f71a6b814af33c
Yuri <50186716+Yuricst@users.noreply.github.com>
2022-03-18 18:44:18 -0400

    Update _elements2statevec.py

--- polaris/Keplerian/_elements2statevec.py
+++ polaris/Keplerian/_elements2statevec.py
@@ -7,42 +7,42 @@ import numpy.linalg as la
 from numba import jit
 
 try:
-	from ._rotationMatrices import rotmat1, rotmat3
+    from ._rotationMatrices import rotmat1, rotmat3
 except:
-	from _rotationMatrices import rotmat1, rotmat3
+    from _rotationMatrices import rotmat1, rotmat3
 
 
 def elts2sv(elts_dict, mu):
-	"""
-	Convert keplerian elements to state-vector
+    """
+    Convert keplerian elements to state-vector
 
-	Args:
-		elts_dict (dict): dictionary containing "sma", inc", "ecc", "raan", "aop", "ta". Angles in radians.
-		mu (float): gravitational parameter
+    Args:
+        elts_dict (dict): dictionary containing "sma", inc", "ecc", "raan", "aop", "ta". Angles in radians.
+        mu (float): gravitational parameter
 
-	Returns:
-		(np.array): state-vector
-	"""
-	# extract from dictionary
-	sma  = elts_dict["sma"]
-	inc  = elts_dict["inc"]
-	ecc  = elts_dict["ecc"]
-	raan = elts_dict["raan"]
-	aop  = elts_dict["aop"]
-	ta   = elts_dict["ta"]
-	assert ecc < 1.0
-	# run jit-ed function
-	return __elts2sv(sma, inc, ecc, raan, aop, ta, mu)
+    Returns:
+        (np.array): state-vector
+    """
+    # extract from dictionary
+    sma  = elts_dict["sma"]
+    inc  = elts_dict["inc"]
+    ecc  = elts_dict["ecc"]
+    raan = elts_dict["raan"]
+    aop  = elts_dict["aop"]
+    ta   = elts_dict["ta"]
+    assert ecc < 1.0
+    # run jit-ed function
+    return __elts2sv(sma, inc, ecc, raan, aop, ta, mu)
 
 
 
 @jit(nopython=True)
 def __elts2sv(sma, inc, ecc, raan, aop, ta, mu):
-	# construct state in perifocal frame
-	h = np.sqrt(sma*mu*(1 - ecc**2))
-	rPF = h**2 / (mu*(1 + ecc*np.cos(ta))) * np.array([np.cos(ta), np.sin(ta), 0.0])
-	vPF = mu/h * np.array([-np.sin(ta), ecc + np.cos(ta), 0.0])
-	# convert to inertial frame
+    # construct state in perifocal frame
+    h = np.sqrt(sma*mu*(1 - ecc**2))
+    rPF = h**2 / (mu*(1 + ecc*np.cos(ta))) * np.array([np.cos(ta), np.sin(ta), 0.0])
+    vPF = mu/h * np.array([-np.sin(ta), ecc + np.cos(ta), 0.0])
+    # convert to inertial frame
     if (aop != 0.0):
         r1 = np.dot(rotmat3(-aop), rPF)
         v1 = np.dot(rotmat3(-aop), vPF)
@@ -63,4 +63,4 @@ def __elts2sv(sma, inc, ecc, raan, aop, ta, mu):
     else:
         rI = r2
         vI = v2
-	return np.concatenate((rI, vI))
+    return np.concatenate((rI, vI))
